1e3570fe45235f1b360923b5b3a304ec0762c506
jingwenzheng <alphabetion@gmail.com>
2022-06-30 10:11:34 +0800

    feat: 目录结构以及文件调整

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import jsx from "@vitejs/plugin-vue-jsx";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [vue(), jsx()],
+  build: {
+    rollupOptions: {
+      input: {
+        record: resolve(__dirname, "./record/index.html"),
+        replay: resolve(__dirname, "./replay/index.html"),
+      },
+    },
+  },
+});
